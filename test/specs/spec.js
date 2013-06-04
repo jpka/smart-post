0f932d9acafa44101d349da684f61d6d20961c4a
Juan Pablo Kaniefsky <jpkaniefsky@gmail.com>
2013-06-04 20:38:33 -0300

    now accepts the model as an attribute

--- test/specs/spec.js
+++ test/specs/spec.js
@@ -1,25 +1,15 @@
 describe("smart-post", function() {  
   var element;
 
-  beforeEach(function() {
-    element = fixtures.window().document.createElement("jpka-smart-post");
+  before(function() {
+    element = fixtures.window().document.querySelector("#post");
   });
 
   it("contains a body", function() {
     expect(element.webkitShadowRoot.querySelector("#body")).to.exist;
   });
 
-  it("can get and set the model", function() {
-    var model = {a: 1};
-    element.model = model;
-    expect(element.model).to.deep.equal(model);
-  });
-
-  it("updates the body when a model with data is added", function(done) {
-    element.model = {body: "Default body"};
-    setTimeout(function() {
-      expect(element.webkitShadowRoot.querySelector("#body").innerHTML).to.equal("Default body");
-      done();
-    }, 1000);
+  it("can be instantiated with model as an attribute", function() {
+    expect(element.model).to.exist.and.to.deep.equal({body: "body"});
   });
 });
